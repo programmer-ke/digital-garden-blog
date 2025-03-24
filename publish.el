b941564e591068e5a74662dca0a7cc79cf29400e
Kenny Rch <ken@99nth.com>
2025-03-24 15:47:37 +0300

    refactor: Use parameterized variable for public directory

--- publish.el
+++ publish.el
@@ -157,7 +157,7 @@ PROJECT: `posts in this case."
 (defun blog/generate-feed (plist)
   (webfeeder-build
    "atom.xml"
-   "public/"
+   "public/"  # Use parameterized variable here AI!
    "https://digitalgarden.ken.ke/"
    (cl-remove-if
     (lambda (f) (member f '("index.html" "posts.html")))
